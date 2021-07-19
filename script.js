3dcf470358ed719ae0d88c6899eff273ef4f6fc0
memojtabazamani <memojtabazamani@gmail.com>
2021-07-19 17:43:34 +0430

    done: استفاده از پراپرتی validity جهت پیغام سفارشی

--- script.js
+++ script.js
@@ -5,11 +5,20 @@ var ageText = document.getElementById("ageText");
 var ageTextHelpBlock = document.getElementById("ageTextHelpBlock");
 
 function validationForm(e) {
-	helpBlock.innerHTML = ageTextHelpBlock.innerHTML = "";
-	if(!userNameText.checkValidity()) {
-		helpBlock.innerHTML = userNameText.validationMessage;
+	message = helpBlock.innerHTML = ageTextHelpBlock.innerHTML = "";
+	if(userNameText.validity.valueMissing) {
+		message = "Field must be entred";	
+	} else if(userNameText.validity.patternMismatch) {
+		message = "Field Pattern in invalid!";
+	} else if(userNameText.validity.rangeOverflow) {
+		message = "Field Greater than its max number.";
+	} else if(userNameText.validity.rangeUnderflow) {
+		message = "Field Less than its min.";
+	} else if(userNameText.validity.toLong) {
+		message = "Field value exceeds its maxLength attribute.";
 	}
-	if(!ageText.checkValidity()) {
-		ageTextHelpBlock.innerHTML = ageText.validationMessage;
-	}
+
+	userNameText.setCustomValidity(message);
+	helpBlock.innerHTML = userNameText.validationMessage;
+	
 }
